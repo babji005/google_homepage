151b8ec4f8bde48560f0ee9cecf7a2809ed22bb8
babji005 <babjipbd@gmail.com>
2022-09-11 10:48:11 +0530

    first commit

---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+// <!DOCTYPE html>
+
+import React, { Component } from "react";
+import "./Logo.css";
+
+
+export default class Logo extends React.Component {
+  
+
+  render() {
+    return (
+      <div>
+        <div id="google"></div>
+      </div>
+    );
+  }
+}
